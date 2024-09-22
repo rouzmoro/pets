bafc9a9fcaba5e25d9c6bc10f09013988765b8dd
rouzmoro <rouzmoro@gmail.com>
2024-09-22 12:09:10 +0500

    Revert "8"
    This reverts commit 87553a2ea15d475ea540b47d1b722fce8ef8221f.

--- js/masonryBlog.js
+++ js/masonryBlog.js
@@ -7,12 +7,11 @@ let msnry = new Masonry(elem, {
     percentPosition: true,
 });
 
-/*let mutantor = document.querySelector('#chat-wrapper');
+let mutantor = document.querySelector('#chat-wrapper');
 
 let observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         if (mutation.type === 'childList' || mutation.type === 'characterData') {
-            console.log('not');
             msnry.layout();
         }
     });
@@ -22,21 +21,6 @@ observer.observe(mutantor, {
     childList: true,
     characterData: true,
     subtree: true
-});*/
-
-let comments = document.querySelectorAll('.chat-textarea');
-
-for (let comment of comments) {
-    comment.addEventListener('input', () => {
-        resizeTextarea(comment);
-        msnry.layout();
-    })
-}
-
-function resizeTextarea(textarea) {
-    textarea.style.height = 'auto';
-    textarea.style.height = textarea.scrollHeight + "px";
-}
+});
 
 window.addEventListener("load", () => msnry.layout());
-/*window.addEventListener('resize', () => resizeTextarea(textarea));*/
